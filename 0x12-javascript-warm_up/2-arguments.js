fc6e3ef29ac58536db06e05903f31dff05038d5d
Bensonodhiambo <bensonodhiambo.bo@gmail.com>
2024-04-08 19:49:47 -0700

    Task2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,2 @@
+#!/usr/bin/node
+if (process.arg.length === 2) { console.log('No argument'); } else if (process.arg.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
